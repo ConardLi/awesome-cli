a079ced5e65bc0d1138e0a4e0c5f28c17d548604
ConardLi <1009903985@qq.com>
2019-09-24 22:34:59 +0800

    :art: update: code-cc

--- code-complexity/index.js
+++ code-complexity/index.js
@@ -1,15 +1,9 @@
 /**
- * todo
- * - gitignore 
- * - babel
- * - publish
+ * 代码复杂度检测
  */
 
-const path = require('path');
-const fs = require('fs');
 const eslint = require('eslint');
-const glob = require('glob');
-const ignore = require('ignore');
+const scan = require('c-scan');
 
 
 const { CLIEngine } = eslint;
@@ -40,28 +34,6 @@ const MESSAGE_PREFIX = 'Maximum allowed is 1.';
  * eslint提示后缀
  */
 const MESSAGE_SUFFIX = 'has a complexity of ';
-
-/**
- * 默认扫描扩展
- */
-const EXTENSIONS = '**/*.js';
-
-/**
- * 默认忽略文件夹
- */
-const DEFAULT_IGNORE_PATTERNS = [
-    'node_modules/**',
-    'build/**',
-    'dist/**',
-    'output/**',
-    'common_build/**'
-];
-
-/**
- * ignore文件名
- */
-const IGNORE_FILE_NAME = '.gitignore';
-
 
 /**
  * 提取mssage主要部分
@@ -125,7 +97,7 @@ function getAdvice(complexity) {
 /**
  * 获取单个文件的复杂度
  */
-function executeOnFiles(paths, min = 10) {
+function executeOnFiles(paths, min) {
     const reports = cli.executeOnFiles(paths).results;
     const result = [];
     for (let i = 0; i < reports.length; i++) {
@@ -134,7 +106,7 @@ function executeOnFiles(paths, min = 10) {
             const { message, ruleId, line, column } = messages[j];
             if (ruleId === 'complexity') {
                 const complexity = getComplexity(message);
-                if (complexity > min) {
+                if (complexity >= min) {
                     result.push({
                         funcType: getFunctionType(message),
                         funcName: getFunctionName(message),
@@ -147,78 +119,18 @@ function executeOnFiles(paths, min = 10) {
             }
         }
     }
-}
-
-/**
- * 获取glob扫描的文件列表
- * @param {*} path 
- */
-function getGlobScan(path = '') {
-    return new Promise(resolve => {
-        glob(`${path}${EXTENSIONS}`,
-            { dot: true, ignore: DEFAULT_IGNORE_PATTERNS },
-            (err, files) => {
-                if (err) {
-                    console.log(err);
-                    process.exit(1);
-                }
-                resolve(files);
-            });
-    });
-}
-
-/**
- * 加载ignore配置文件，并处理成数组
- * @param {*} ignoreFileName 
- */
-async function loadIgnorePatterns(ignoreFileName) {
-    const ignorePath = path.resolve(process.cwd(), ignoreFileName);
-    try {
-        const ignores = fs.readFileSync(ignorePath, 'utf8');
-        return ignores.split(/[\n\r]|\n\r/).filter(pattern => Boolean(pattern));
-    } catch (e) {
-        return [];
-    }
-}
-
-/**
- * 根据ignore配置过滤文件列表
- * @param {*} files 
- * @param {*} ignorePatterns 
- * @param {*} cwd 
- */
-function filterFilesByIgnore(files, ignorePatterns, cwd = process.cwd()) {
-    const ig = ignore().add(ignorePatterns);
-    const filtered = files
-        .map(raw => (path.isAbsolute(raw) ? raw : path.resolve(cwd, raw)))
-        .map(raw => path.relative(cwd, raw))
-        .filter(filePath => !ig.ignores(filePath))
-        .map(raw => path.resolve(cwd, raw));
-
-    return filtered;
+    return result;
 }
 
 /**
  * 执行扫描
  * @param {*} path 扫描路径 - 默认为当前路径
- * @param {*} min 最小代码扫描复杂度
+ * @param {*} min 最小代码复杂度 , 大于此值不会被添加到结果
  */
-async function scan(path = '', min = 10) {
+module.exports = async function (scanParam = {}, min = 1) {
 
-    console.time();
+    const files = await scan(scanParam);
 
-    const ignorePatterns = await loadIgnorePatterns(IGNORE_FILE_NAME);
-
-    let files = await getGlobScan(path);
-
-    files = filterFilesByIgnore(files, ignorePatterns);
-
-    executeOnFiles(files, min);
-
-    console.timeEnd();
+    return executeOnFiles(files, min);
 
 }
-
-scan();
-
-// executeOnFile('../src');
