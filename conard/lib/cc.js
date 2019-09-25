a3139a436648611b68429b0f9439fcfa3a291b1e
ConardLi <1009903985@qq.com>
2019-09-25 21:49:22 +0800

    :sparkles: add: logger

--- conard/lib/cc.js
+++ conard/lib/cc.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 const cc = require('c-complexity');
+const logger = require('../utils/logger');
 
 
 module.exports = async function (param) {
-    console.log('正在执行检测');
+
+    logger.loading('正在执行检测');
+
     const start = Date.now();
+
     const {
         min,
         rootPath = '',
@@ -18,11 +22,16 @@ module.exports = async function (param) {
         ignoreFileName
     }, min);
 
+    logger.stop();
+
     const { fileCount, funcCount, result } = ccResult;
-    console.log(`检测完成,耗费${Date.now() - start}ms，共检测【${fileCount}】个文件，【${funcCount}】个函数，其中可能存在问题的函数【${result.length}】个`);
+
+    logger.success(`检测完成,耗费${Date.now() - start}ms，共检测【${fileCount}】个文件，【${funcCount}】个函数，其中可能存在问题的函数【${result.length}】个`);
+
     if (result.length) {
-        console.table(result);
+        logger.table(result);
     } else {
-        console.log('你的代码非常棒！');
+        logger.info('你的代码非常棒！');
     }
+
 };
---
+++ conard/utils/logger.js
@@ -0,0 +1,51 @@
+const { Signale } = require('signale');
+const chalk = require('chalk');
+const { Spinner } = require('cli-spinner');
+
+let spinner = new Spinner('');
+
+
+const options = {
+    types: {
+        error: {
+            badge: ' ❌ ',
+            color: 'red',
+            label: '失败'
+        },
+        success: {
+            badge: ' 🎉 ',
+            color: 'green',
+            label: '成功'
+        },
+        info: {
+            badge: '',
+            color: '',
+            label: ''
+        }
+    }
+};
+
+const signale = new Signale(options);
+
+
+module.exports = {
+    success: function (msg) {
+        signale.success(chalk.green(msg));
+    },
+    error: function (msg) {
+        signale.error(chalk.red(msg));
+    },
+    info: function (msg) {
+        signale.info(chalk.blue(msg));
+    },
+    loading: (title = '加载中...') => {
+        spinner.setSpinnerTitle(` 💫  ${title}  %s`);
+        spinner.setSpinnerString('⣾⣽⣻⢿⡿⣟⣯⣷');
+        spinner.start();
+    },
+    stop: () => {
+        spinner.stop();
+        console.log();
+    },
+    talbe: console.table
+};
