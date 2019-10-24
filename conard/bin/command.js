72b35eb0d9c5558736ea5475628a381a6711131e
ConardLi <1009903985@qq.com>
2019-10-24 21:27:03 +0800

    fix: add: version

--- conard/bin/command.js
+++ conard/bin/command.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const pkg = require('../package.json');
 
 const cc = require('../lib/cc');
 const cl = require('../lib/cl');
@@ -6,8 +7,22 @@ const cl = require('../lib/cl');
 
 module.exports = function () {
 
+  /**
+   * 代码复杂度
+   */
   yargs.command('cc', '', {}, cc);
+
+  /**
+   * 代码行数
+   */
   yargs.command('cl', '', {}, cl);
+
+  /**
+   * 版本
+   */
+  yargs.command('version', '', {}, () => {
+    console.log(pkg.version);
+  });
 
   yargs.argv;
 
